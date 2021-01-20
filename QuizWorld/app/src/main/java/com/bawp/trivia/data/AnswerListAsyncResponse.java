79f888ab7ea431077f9d31d55041e2f6f7ced2e3
Crystal <dltnwjd483@gmail.com>
2021-01-20 13:29:16 +0900

    remove files

--- QuizWorld/app/src/main/java/com/bawp/trivia/data/AnswerListAsyncResponse.java
+++
@@ -1,9 +0,0 @@
-package com.bawp.trivia.data;
-
-import com.bawp.trivia.model.Question;
-
-import java.util.ArrayList;
-
-public interface AnswerListAsyncResponse {
-    void processFinished(ArrayList<Question> question);
-}
